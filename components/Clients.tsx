32422a8b7f32e2635b4823a264c9a64509b554cd
Sachin-0001 <sachin.samprit@gmail.com>
2025-06-28 00:58:32 +0530

    fixed dep issues

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -2,7 +2,50 @@
 
 import React from "react";
 
-import { companies, testimonials } from "@/data";
+// Local copy of companies and testimonials since they are commented out in data/index.ts
+const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  // Add more testimonials as needed
+];
+
+const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
 import { InfiniteMovingCards } from "./ui/InfiniteCards";
 
 const Clients = () => {
